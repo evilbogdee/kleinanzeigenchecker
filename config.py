cef3469c397424b014974728ca19ed3723191881
evilbogdee <levxichnov@gmail.com>
2024-11-24 15:18:29 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# files
+PROXY_FILE_PATH = os.path.join(PROJECT_ROOT, "proxies", "proxies.txt") # proxies - название папки с прокси, proxies.txt - файл с прокси
+COOKIES_FILE_PATH = os.path.join(PROJECT_ROOT, "cookies") # cookies - название папки с куки
+
+# urls
+SMS_URL = "https://www.kleinanzeigen.de/m-nachrichten.html"
+# PROFILE_URL = "https://www.kleinanzeigen.de/m-meine-anzeigen.html"
+PROFILE_URL = "https://www.kleinanzeigen.de/"
